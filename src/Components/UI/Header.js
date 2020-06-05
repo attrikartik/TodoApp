ad1c3611a4176084c5254a02bce79091c49024e2
Kartik <kartik.attri@vtnetzwelt.com>
2020-06-05 18:41:08 +0530

    second commit

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Toolbar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
